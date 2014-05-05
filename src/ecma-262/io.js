42d9dd51aca2767c62d18b04e5014626f5afc5c6
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2014-05-05 14:24:49 -0300

    exemplo de io

---
+++ src/ecma-262/io.js
@@ -0,0 +1,9 @@
+#Input
+var value = window.prompt("Digite algum valor:");
+var value = document.querySelector("#id").value;
+
+#Output
+alert("Exibe caixa");
+console.log("Exibe no console");
+document.write("Exibe no documento");
+document.querySelector("#id").value = "valor";
