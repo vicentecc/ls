53ad2a283711188be2502c2266770ac80b46b762
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2014-05-12 14:30:10 -0300

    exemplos de loop e lista de api

--- src/ecma-262/io.js
+++ src/ecma-262/io.js
@@ -1,9 +1,11 @@
 // Input
 var value = window.prompt("Digite algum valor:");
-var value = document.querySelector("#id").value;
+document.querySelector("#idInputText").value = "valor";
+document.querySelector("#div").innerHTML = "Algum texto";
 
 // Output
 alert("Exibe caixa");
 console.log("Exibe no console");
 document.write("Exibe no documento");
-document.querySelector("#id").value = "valor";
+var value = document.querySelector("#idInputText").value;
+var value = document.querySelector("#div").innerHTML;
