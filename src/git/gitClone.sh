30e9a461d9c0cfe5887e96c4faedd1332007bc7c
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2014-05-19 14:19:08 -0300

    script clone github

---
+++ src/git/gitClone.sh
@@ -0,0 +1,8 @@
+# Antes de executar ./gitClone.sh habilite a execução através de
+# chmod +x gitClone.sh
+# e garanta que não exista alguma pasta ls no Desktop
+
+cd ~/Desktop
+mkdir ls
+cd ls
+git clone https://github.com/ifpb/ls.git
